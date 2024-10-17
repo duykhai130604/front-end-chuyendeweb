204ff4f43cb908bda941cdee7a490722d8e3bf61
nguyenphucthiennhan <nguyenphucthiennhan3358@gmail.com>
2024-10-17 15:33:34 +0700

    axios

--- src/router/user.js
+++ src/router/user.js
@@ -3,13 +3,6 @@ const home = [
     {
         path: "/",
         component: () => import("../views/user/HomeView.vue"),
-        children: [
-            {
-                path: "home",
-                name: "user-home",
-                component: () => import("../pages/user/HomePage.vue"),
-            },
-        ],
     }
 ];
 export default home;
