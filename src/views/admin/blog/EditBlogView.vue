886c426a3d61d109138e386cd7fdc0ff9ceb86e4
nguyenphucthiennhan <nguyenphucthiennhan3358@gmail.com>
2024-10-21 16:33:12 +0700

    fix syntax

--- src/views/admin/blog/EditBlogView.vue
+++ src/views/admin/blog/EditBlogView.vue
@@ -18,13 +18,13 @@
 </template>
 
 <script>
-import "../../../assets/admin/assets/js/plugin/webfont/webfont.min";
+import "../../../assets/admin/assets/js/plugin/webfont/webfont.min.js";
 import "../../../assets/admin/assets/css/bootstrap.min.css";
 import "../../../assets/admin/assets/css/kaiadmin.min.css";
 import "../../../assets/admin/assets/css/demo.css";
 import "../../../assets/admin/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js";
-import "../../../assets/admin/assets/js/core/bootstrap.min";
-import "../../../assets/admin/assets/js/core/jquery-3.7.1.min";
+import "../../../assets/admin/assets/js/core/bootstrap.min copy.js";
+ import "../../../assets/admin/assets/js/core/jquery-3.7.1.min copy.js";
 import EditBlog from "@/components/admin/blog/EditBlog.vue"; 
 import NavbarComponent from "@/components/admin/NavbarComponent.vue";
 import HeaderComponent from "@/components/admin/HeaderComponent.vue";
