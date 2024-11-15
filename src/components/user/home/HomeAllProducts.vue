03d38344b0bef3d26fe9f767136f88edb9a37b2a
nguyenphucthiennhan <nguyenphucthiennhan3358@gmail.com>
2024-11-15 22:00:52 +0700

    manage order was successful

--- src/components/user/home/HomeAllProducts.vue
+++ src/components/user/home/HomeAllProducts.vue
@@ -108,7 +108,7 @@
           </div>
         </div>
         <div class="flex-c-m flex-w w-full p-t-45" v-if="hasMoreSProducts && !loadingS">
-          <button @click="loadMoreS" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
+          <button  @click="loadMoreS" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
             Load More
           </button>
         </div>
--- src/components/user/order/UserOrder.vue
+++ src/components/user/order/UserOrder.vue
@@ -11,6 +11,7 @@
                         <th>Tên sản phẩm</th>
                         <th>Giá</th>
                         <th>Thời gian</th>
+                        <th>Tình trạng</th>
                         <th>Hành động</th>
                     </tr>
                 </thead>
@@ -27,11 +28,17 @@
                         <td>{{ product.name }}</td>
                         <td class="product-price">{{ product.total }} VND</td>
                         <td class="product-time">{{ formatDate(product.created_at) }} </td>
+                        <td class="product-time">{{ getStatusText(product.status) }} </td>
                         <td>
                             <button @click="viewProductDetail(product.id)" class="view-detail-btn">Xem chi tiết</button>
-                            <button v-if="product.rate == 0"
+                            <button v-if="product.rate == 0 && product.status == 1"
                                 @click="reviewProduct(product.product, product.variant, product.order)"
                                 class="review-btn">Đánh giá</button>
+                            <button v-if="product.status == 3" @click="gotProduct(product.id)"
+                                class="btn btn-warning mx-2">Đã nhận được
+                                hàng</button>
+                            <button v-if="product.status == 3" @click="chooseReason(product.id)"
+                                class="btn btn-danger mx-2 my-2">Hủy đơn</button>
                         </td>
                     </tr>
                 </tbody>
@@ -39,7 +46,32 @@
             <button @click="loadMore" v-if="hasMore" class="load-more-btn btn">Tải thêm</button>
         </div>
     </div>
-
+    <!-- Modal structure -->
+    <div v-if="isModalVisible" class="modal-overlay" @click="closeModal">
+        <div class="modal-content" @click.stop>
+            <h2>Lí do hủy đơn</h2>
+            <div class="form-group">
+                <label for="reasonList">Chọn lý do:</label>
+                <select class="form-control" id="reasonList" v-model="reason">
+                    <option v-for="(reasonOption, index) in commonReasons" :key="index" :value="reasonOption">
+                        {{ reasonOption }}
+                    </option>
+                    <option value="Khác">Khác (nhập bên dưới)</option>
+                </select>
+            </div>
+            <!-- Ô nhập lý do khác -->
+            <div v-if="reason === 'Khác'" class="form-group">
+                <label for="otherReason">Nhập lý do khác:</label>
+                <input type="text" class="form-control" id="otherReason" v-model="otherReason"
+                    placeholder="Nhập lý do hủy đơn của bạn" />
+            </div>
+            <!-- Close Button -->
+            <div class="btn btn-secondary" @click="closeModal">Close
+            </div>
+            <div class="btn btn-danger my-2" @click="cancle()">Hủy
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 import axios from 'axios';
@@ -50,9 +82,99 @@ export default {
             purchasedProducts: [],
             page: 1,
             hasMore: true,
+            reason: '',
+            isModalVisible: false,
+            order: null,
+            otherReason: '',
+            commonReasons: [
+                'Đặt nhầm sản phẩm',
+                'Thay đổi địa chỉ giao hàng',
+                'Thời gian giao hàng quá lâu',
+                'Sản phẩm không còn cần thiết',
+                'Giá sản phẩm không hợp lý',
+                'Tìm thấy sản phẩm giá tốt hơn',
+                'Sản phẩm không giống mô tả',
+                'Phí vận chuyển quá cao',
+                'Không liên hệ được với nhân viên',
+                'Khác'
+            ]
+
         };
     },
     methods: {
+        validateCancellation() {
+            if (!this.reason) {
+                alert('Vui lòng chọn lý do hủy đơn.');
+                return false;
+            }
+            if (this.reason === 'Khác' && (!this.otherReason || this.otherReason.length > 150)) {
+                alert(
+                    'Vui lòng nhập lý do khác không quá 150 ký tự?'
+                );
+                return false;
+            }
+            return true;
+        },
+        chooseReason(order) {
+            this.order = order;
+            this.isModalVisible = true;
+        },
+        closeModal() {
+            this.isModalVisible = false;
+        },
+        //cancle
+        async cancle() {
+            try {
+                if (this.validateCancellation()) {
+                    if (confirm('Bạn có chắc hủy đơn này?')) {
+                        if (this.reason == 'Khác'){
+                            await axios.put(API_BASE_URL + '/product-order/update-status', {
+                                id: this.order,
+                                status: 2,
+                                reason: this.otherReason,
+                            });
+                        }else{
+                            await axios.put(API_BASE_URL + '/product-order/update-status', {
+                                id: this.order,
+                                status: 2,
+                                reason: this.reason,
+                            });
+                        }
+                        this.purchasedProducts = [];
+                        this.isModalVisible = false;
+                        this.fetchProductOrders();
+
+                    }
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    console.error('Order not found or invalid status');
+                } else {
+                    console.error('Error updating order:', error);
+                }
+
+            }
+        },
+        async gotProduct(order) {
+            try {
+                if (confirm('Bạn đã nhận được hàng?')) {
+                    await axios.put(API_BASE_URL + '/product-order/update-status', {
+                        id: order,
+                        status: 1,
+                        reason: null,
+                    });
+                    this.purchasedProducts = [];
+                    this.fetchProductOrders();
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    console.error('Order not found or invalid status');
+                } else {
+                    console.error('Error updating order:', error);
+                }
+
+            }
+        },
         fetchProductOrders() {
             axios
                 .get(`${API_BASE_URL}/user/orders?page=${this.page}`, {
@@ -60,7 +182,6 @@ export default {
                 })
                 .then(response => {
                     const data = response.data;
-                    console.log(response.data);
                     this.purchasedProducts.push(...data.data);
                     this.hasMore = data.current_page < data.last_page;
                 })
@@ -99,6 +220,22 @@ export default {
                 hour12: false
             };
             return date.toLocaleDateString('vi-VN', options);
+        },
+        getStatusText(statusCode) {
+            switch (statusCode) {
+                case 1:
+                    return 'Đã giao thành công';
+                case 0:
+                    return 'Bị hủy bởi người bán';
+                case 2:
+                    return 'Bị hủy bởi khách hàng';
+                case 3:
+                    return 'Đang chuyển xử lý';
+                case 4:
+                    return 'Thanh toán hoặc hóa đơn xử lý không thành công';
+                default:
+                    return 'Không xác định';
+            }
         },
     },
     created() {
@@ -197,5 +334,26 @@ tr {
     border-radius: 4px;
     cursor: pointer;
     transition: background-color 0.3s ease;
+}
+
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
+}
+
+.modal-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    width: 600px;
+    box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
 }
 </style>
