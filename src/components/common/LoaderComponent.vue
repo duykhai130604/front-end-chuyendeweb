1bd145db8931945ed6b9c7d61b33285b28c3dc52
nguyenphucthiennhan <nguyenphucthiennhan3358@gmail.com>
2024-11-16 21:59:36 +0700

    done

---
+++ src/components/common/LoaderComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <div v-if="isLoading" class="loader1">
+      <div class="loader1"></div>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: {
+      isLoading: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .loader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+  }
+  .loader1 {
+    border: 5px solid rgba(255, 255, 255, 0.3);
+    border-top: 5px solid #fff;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 1s linear infinite;
+  }
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  </style>
+  
